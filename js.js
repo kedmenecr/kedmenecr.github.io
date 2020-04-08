7b333a07f51ca591432c346f50f6c7bfbf44912a
Roberto Kedmenec <rkedmenec@tria.hr>
2020-04-08 23:14:52 +0200

    Fuck it,ground beef.

--- js.js
+++ js.js
@@ -5,6 +5,23 @@ var txt = 'Who am I?'; /* The text */
 var txt2 = 'Who am I?'; /* The text */
 var speed = 250; /* The speed/duration of the effect in milliseconds */
 
+function clearBody() {
+    setTimeout(() => {
+        document.body.innerHTML = ''
+        var par = document.createElement("span");
+        var hereAnchro = document.createElement("a");
+        hereAnchro.setAttribute('href', 'me.html');
+        par.setAttribute("class", "blinking")
+        hereAnchro.setAttribute("class", "blinking")
+        var text = document.createTextNode("Find Out");
+        var textHere = document.createTextNode("Here.");
+        par.appendChild(text);
+        hereAnchro.appendChild(textHere)
+        document.body.appendChild(par);
+        document.body.appendChild(hereAnchro);
+    }, 2000);
+}
+
 function typeWriter() {
     if (i < txt.length) {
         document.getElementById("me").innerHTML += txt.charAt(i);
@@ -14,6 +31,7 @@ function typeWriter() {
         i2++;
         setTimeout(typeWriter, speed);
     } else {
+        clearBody();
     }
 }
 typeWriter()
